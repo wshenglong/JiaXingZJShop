9fbe6bbb1feae85ed07495a64aa614928e173577
jsonshenglong <jsonshenglong@gmail.com>
2019-01-15 00:13:31 +0800

    swift4.2 init project,test first ok

---
+++ JiaXingZJShop/JiaXingZJShop/JiaXingZJShop-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  JiaXingZJShop-Bridging-Header.h
+//  JiaXingZJShop
+//
+//  Created by jsonshenglong on 2019/1/14.
+//  Copyright © 2019年 jsonshenglong. All rights reserved.
+//
+
+#import "SVProgressHUD.h"
+#import "UIImageView+WebCache.h"
+#import "MJRefresh.h"
+//#import "UMSocial.h"
+#import <CoreLocation/CoreLocation.h>
+//#import "UMSocialSinaSSOHandler.h"
+#import "UMSocialWechatHandler.h"
+#import "UMSocialQQHandler.h"
+#import "MJExtension.h"
+
